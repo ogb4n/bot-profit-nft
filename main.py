9fe286df7c3dbb6d001121a72871397c9099be45
reapex <reapex50@proton.me>
2023-04-15 19:05:11 +0200

    del-wallet

--- main.py
+++ main.py
@@ -53,19 +53,55 @@ async def addwallet(ctx, wallet_id=None, wallet_address=None):
 async def delwallet(ctx, wallet_id=None):
     """Permet d'ajouter un wallet"""
     try:
-        cur.execute(f"SELECT * FROM users WHERE discord_id = {ctx.author.id}")
+        cur.execute(f"SELECT * FROM users WHERE discord_id = {ctx.author.id};")
         result = cur.fetchone()
         if not result:
-            await ctx.send(f"oui ça marche {ctx.author.name}, tu possèdes l'id {ctx.author.id}, tu essaies d'ajouter le wallet {wallet_address} a ton wallet n°{wallet_id}")
             cur.execute(f"INSERT INTO users(discord_id, wallet_{wallet_id}) VALUES ({ctx.author.id}, 'none');")
             db.commit()
         else:
             cur.execute(f"UPDATE users SET wallet_{wallet_id} = 'none' WHERE discord_id = {ctx.author.id}")
             db.commit()
-            logger.addInfo(f"Le wallet a bien été supprimé de la liste de {ctx.author.name}")
-            await ctx.send(f"tu as bien supprimé le wallet a ta liste")
+        logger.addInfo(f"Le wallet a bien été supprimé de la liste de {ctx.author.name}")
+        await ctx.send(f"tu as bien supprimé le wallet a ta liste")
     except Exception as e:
         print(e)
+
+@bot.command()
+async def infos(ctx):
+    """affiche les informations de l'utilisateur"""
+    try: 
+        cur.execute(f"SELECT * FROM users WHERE discord_id = {ctx.author.id};")
+        row = cur.fetchone()
+        if not row:
+            await ctx.send("l'utilisateur n'a aucune information associée")
+            return
+        result = discord.Embed(
+            color=0xB44E7F,
+            title= f"My infos Page",
+            description=f"<@{row[1]}>'s profile"
+        )
+        result.add_field(
+            name= "My wallets",
+            inline=False,
+            value=
+            f"""Wallet n°1 : {row[2]}
+                Wallet n°2 : {row[3]}
+                Wallet n°3 : {row[4]}
+                Wallet n°4 : {row[5]}
+                Wallet n°5 : {row[6]}
+                Wallet n°6 : {row[7]}
+                Wallet n°7 : {row[8]}
+                Wallet n°8 : {row[9]}
+                Wallet n°9 : {row[10]}
+                Wallet n°10 : {row[11]}"""
+                )
+        if not result:
+            await ctx.send("l'utilisateur n'a aucune information associée")
+        else:
+            await ctx.send(embed=result)
+    except Exception as e:
+        print(e)
+
 
 if __name__ == '__main__':
     args = getArgs()
